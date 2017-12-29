0a890ea9f27563cc6a496c6ee65fd35a0f61fd72
Kris Thompson <kt.devcat@gmail.com>
2017-12-29 17:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser = "Kris";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
